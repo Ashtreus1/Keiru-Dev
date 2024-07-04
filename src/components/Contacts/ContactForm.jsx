de127a4564881cb49f2e757768599e28a73d8671
Jameson Gelarman <96761338+Ashtreus1@users.noreply.github.com>
2024-07-04 06:23:13 +0000

    add some latest projects

--- src/components/Contacts/ContactForm.jsx
+++ src/components/Contacts/ContactForm.jsx
@@ -1,7 +1,8 @@
 "use client"
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { motion, useInView } from 'framer-motion';
 import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import emailjs from '@emailjs/browser';
 
 const formVariants = {
   hidden: { opacity: 0, y: 20 },
@@ -18,9 +19,46 @@ const socialLinkVariants = {
   visible: { opacity: 1, x: 0, transition: { duration: 0.6, ease: 'easeOut', staggerChildren: 0.2 } }
 };
 
+
 const ContactForm = () => {
   const ref = React.useRef(null);
   const isInView = useInView(ref, { once: true });
+
+  const form = useRef();
+
+  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+  const [status, setStatus] = useState('');
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormState({ ...formState, [name]: value });
+  };
+
+  // DON'T MODIFY THIS FUNCTION
+  // This function is used to send email using EmailJS
+  // You can find the documentation here: https://www.emailjs.com/docs/
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      'service_anlif28',
+      'template_5t659fm',
+      form.current,
+      {
+        publicKey: 'E_Im3DBTnGaQZusqf'
+      })
+      .then(
+        () => {
+          setStatus('Message sent successfully!');
+          setFormState({ name: '', email: '', message: '' });
+        },
+        (error) => {
+          console.error('Failed to send message:', error);
+          setStatus('Failed to send message. Please try again.');
+        }
+      );
+  };
 
   return (
     <motion.div
@@ -29,13 +67,14 @@ const ContactForm = () => {
       initial="hidden"
       animate={isInView ? 'visible' : 'hidden'}
     >
-      <motion.form variants={formVariants}>
+      <motion.form variants={formVariants} onSubmit={sendEmail} ref={form}>
         <motion.div className="mb-4" variants={formVariants}>
           <label className="input input-bordered rounded-btn flex items-center gap-2">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70">
               <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
             </svg>
-            <input type="text" className="grow" placeholder="Name" />
+            <input type="text" className="grow" name="name" placeholder="Name" value={formState.name}
+              onChange={handleChange} />
           </label>
         </motion.div>
         <motion.div className="mb-4" variants={formVariants}>
@@ -44,17 +83,19 @@ const ContactForm = () => {
               <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
               <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
             </svg>
-            <input type="text" className="grow" placeholder="Email" />
+            <input type="text" className="grow" name="email" placeholder="Email" value={formState.email}
+              onChange={handleChange} />
           </label>
         </motion.div>
         <motion.div className="mb-4" variants={formVariants}>
-          <textarea className="textarea textarea-bordered w-full" placeholder="Message"></textarea>
+          <textarea className="textarea textarea-bordered w-full" placeholder="Message" name="message" value={formState.message}
+            onChange={handleChange}></textarea>
         </motion.div>
         <motion.button className="btn btn-wide hover:bg-cyan-400 hover:text-black transition-color" variants={buttonVariants}>
           Send Message
         </motion.button>
       </motion.form>
-
+      {status && <p className="text-center mt-4">{status}</p>}
       <motion.div className="absolute right-4 mt-5 flex gap-4" variants={socialLinkVariants}>
         <motion.a href="#" className="text-gray-500 hover:text-cyan-500 flex items-center gap-2" variants={socialLinkVariants}>
           <FaFacebook className="w-6 h-6" />
--- src/config/projectData.js
+++ src/config/projectData.js
@@ -2,32 +2,42 @@ const projectData = [
     {
         title: 'Keiru Dev',
         image: 'images/keiru.png',
-        description: 'My official personal portfolio website.',
-        categories: ['Portfolio', 'Personal'],
+        description: 'My official personal portfolio website. Built with ReactJS, TailwindCSS, and hosted on Vercel.',
+        categories: ['Personal Project'],
         live_prev: 'https://keiru.vercel.app/',
         repo_link: 'https://github.com/Ashtreus1/Keiru-Dev',
         tab_categories: ['All', 'Web'],
-        isNew: true
+        isNew: false
     },
     {
-        title: 'Text Summarization Tool Website',
-        image: 'https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg',
-        description: 'Summarization Tool from scratch using the naive summarisation algorithm, built with Laravel Framework',
-        categories: ['Tool', 'Summarization'],
-        tab_categories: ['All', 'Web'],
+        title: 'Virtual Grocery Shop Simulator',
+        image: 'https://scontent.xx.fbcdn.net/v/t1.15752-9/448445223_1646146799530923_1898617109745405079_n.png?_nc_cat=101&ccb=1-7&_nc_sid=0024fc&_nc_eui2=AeEZyaWOR_jBFFoHiuPh33tcdyQWCRym8YR3JBYJHKbxhGhHsnsHIQ_UxayjKPCHQTONWkeU3BPDICmo_crlDt93&_nc_ohc=FvMpET7RVJ8Q7kNvgHejclA&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_Q7cD1QFmDDyIyAgzGx7kzBKqWYQuKF8EOyHWkqBCWrghpQcRjA&oe=66ADB925',
+        description: 'Virtual Grocery Shop Simulator is a Java Swing desktop app that simulates grocery shopping, applying OOP concepts.',
+        categories: ['School Project'],
+        tab_categories: ['All', 'Desktop'],
         live_prev: '',
-        repo_link: 'https://github.com/Ashtreus1/text-summarizer',
-        isNew: true
+        repo_link: 'https://github.com/Ashtreus1/GroceryShop_Simulator',
+        isNew: false
+    },
+    {
+        title: 'Food Restaurant Website (HTML & CSS Only)',
+        image: 'https://scontent.xx.fbcdn.net/v/t1.15752-9/448143940_442106935282443_8168093023177243833_n.png?_nc_cat=109&ccb=1-7&_nc_sid=0024fc&_nc_eui2=AeGNsw_WPQQuG8SrXr2Af8uMYPj6UHMpb8lg-PpQcylvyTNJSnrXONNFbe8SlF1IBNji-AZ6HxuPwZtz-yEeB16M&_nc_ohc=pcyCUUon7qkQ7kNvgEqCPP3&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_Q7cD1QGZ0HndE20hMjM6RjY7Z_RS4NxotYk2VoEJtNA7_y06XQ&oe=66ADB207',
+        description: 'A simple food restaurant website built with HTML and CSS only.',
+        categories: ['School Project'],
+        tab_categories: ['All', 'Website'],
+        live_prev: '',
+        repo_link: 'https://github.com/Ashtreus1/Foodie_Fix',
+        isNew: false
     },
     {
         title: 'TicTacToe',
         image: 'images/tictactoe.png',
-        description: 'A Java GUI 2d Game with AI Integration built ',
-        categories: ['Fitness', 'Health'],
+        description: 'A Java Swing 2d Game with AI Integration built.',
+        categories: ['Side Project'],
         tab_categories: ['All', 'Desktop'],
         live_prev: '',
         repo_link: 'https://github.com/Ashtreus1/TicTacToe',
-        isNew: true
+        isNew: false
     },
 ];
 
